930d7a40881db70cca532d00f143a7f97cad2d2c
vitorPatrick <vitor_patrick@yahoo.com>
2026-03-04 21:33:46 -0300

    Aula 1 criação de dataBase e Tabela usuario

---
+++ Aula - 1/Query criação de tabela usuario.sql
@@ -0,0 +1,10 @@
+USE biblioteca_pessoal_1S2026;
+
+CREATE TABLE Usuario (
+	id_usuario INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    data_cadastro DATE NOT NULL
+);
+
+SHOW TABLES;
---
+++ Aula - 1/query criação de dataBase.sql
@@ -0,0 +1,6 @@
+/*
+ Query para criação de um novo banco de dados
+*/
+CREATE database biblioteca_pessoal_1S2026;
+use biblioteca_pessoal_1S2026;
+Show tables;
