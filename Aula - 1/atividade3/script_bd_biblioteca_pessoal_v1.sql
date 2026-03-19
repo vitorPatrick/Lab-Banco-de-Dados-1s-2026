a72f2c0d38b54bb3ccca3e9f6c38e0397d7bbcf7
vitorPatrick <vitor_patrick@yahoo.com>
2026-03-18 23:05:28 -0300

    Versão final da base de dados

---
+++ Aula - 1/atividade3/script_bd_biblioteca_pessoal_v1.sql
@@ -0,0 +1,58 @@
+CREATE DATABASE biblioteca_pessoal_v1;
+USE biblioteca_pessoal_v1;
+
+CREATE TABLE usuario(
+	id_usuario INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+	senha VARCHAR(255) NOT NULL,
+	data_cadastro TIMESTAMP NOT NULL,
+	data_atualizacao TIMESTAMP NOT NULL
+);
+
+CREATE TABLE categoria(
+	id_categoria INT PRIMARY KEY AUTO_INCREMENT,
+	nome VARCHAR(128) NOT NULL UNIQUE,
+    descricao TEXT,
+    data_cadastro TIMESTAMP,
+    data_atualizacao TIMESTAMP
+);
+
+CREATE TABLE autor(
+	id_autor INT PRIMARY KEY AUTO_INCREMENT,
+	nome VARCHAR(255) NOT NULL UNIQUE,
+    ano_nascimento YEAR NOT NULL,
+    ano_morte YEAR,
+    apresentacao TEXT,
+    data_cadastro TIMESTAMP,
+    data_atualizacao TIMESTAMP
+);
+
+CREATE TABLE editora(
+	id_editora INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(255) NOT NULL UNIQUE,
+	cidade VARCHAR(64) NOT NULL,
+	estado VARCHAR(64) NOT NULL,
+    pais VARCHAR(64) NOT NULL,
+    data_cadastro TIMESTAMP,
+    data_atualizacao TIMESTAMP
+);
+
+CREATE TABLE livro(
+	id_livro INT PRIMARY KEY AUTO_INCREMENT,
+    id_usuario INT NOT NULL,
+    id_autor INT NOT NULL,
+    id_editora INT NOT NULL,
+    id_categoria INT NOT NULL,
+    CONSTRAINT fk_livro_usuario FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario) ON DELETE CASCADE,
+    CONSTRAINT fk_livro_autor FOREIGN KEY (id_autor) REFERENCES autor(id_autor) ON DELETE RESTRICT,
+    CONSTRAINT fk_livro_editora FOREIGN KEY (id_editora) REFERENCES editora(id_editora) ON DELETE RESTRICT,
+    CONSTRAINT fk_livro_categoria FOREIGN KEY (id_categoria) REFERENCES categoria(id_categoria) ON DELETE RESTRICT,
+    titulo VARCHAR(255) NOT NULL,
+    sinopse TEXT,
+    ano_publicacao YEAR,
+    lido BOOLEAN NOT NULL DEFAULT(0),
+    data_cadastro TIMESTAMP,
+    data_atualizacao TIMESTAMP
+);
+DESCRIBE livro;
